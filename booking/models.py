0f7b43799dd3283829de82373b556e4e990fb7ae
Folkor1 <igorvasiljevs@gmail.com>
2022-10-19 18:08:24 +0000

    Add Bookings model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from datetime import datetime
 
-# Create your models here.
+
+class Bookings(models.Model):
+    """
+    Class for Bookings model
+    """
+    lesson = models.CharField(max_length=50)
+    lesson_type = models.CharField(max_length=50)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.BooleanField(default=False)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
