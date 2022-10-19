57614c61a3117e2287ba84770fcd20719dd42aa7
Folkor1 <igorvasiljevs@gmail.com>
2022-10-19 18:32:41 +0000

    Add function to store piano selection

--- static/script/script.js
+++ static/script/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 
+    let booking = [];
+
+    // Change lesson type options when clicked
+    $("#piano-btn").on("click", function() {
+        $("#piano-theory").addClass("d-none");
+        $("#book-for").removeClass("d-none");
+        $("#select-lesson-type").removeClass("d-none");
+        $("#oo-buttons").removeClass("d-none");
+        booking.push('Piano');
+    });
 
 });
