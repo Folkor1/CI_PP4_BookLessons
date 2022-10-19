fc5f806f23ac5f0dbafd2b83106a6178d3fed1fa
Folkor1 <igorvasiljevs@gmail.com>
2022-10-19 18:03:39 +0000

    Add class for bookings model view

--- booking/views.py
+++ booking/views.py
@@ -6,3 +6,10 @@ def homepage(request):
     Render the home page
     """
     return render(request, "index.html")
+
+
+class BookingsView(generic.ListView):
+    """
+    Render bookings page
+    """
+    template_name = "bookings.html"
