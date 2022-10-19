5c29fead16dab5b0c58be4a9aba1da6a4f2fd0e4
Folkor1 <igorvasiljevs@gmail.com>
2022-10-19 20:48:32 +0000

    Add post function in views

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views import generic
 from .models import Bookings
+from django.http import HttpResponseRedirect
 
 
 def homepage(request):
@@ -16,3 +17,18 @@ class BookingsView(generic.ListView):
     """
     model = Bookings
     template_name = "bookings.html"
+
+    def post(self, request):
+        """
+        Create new booking
+        """
+        if request.method == 'POST':
+            lesson = request.POST.get('lesson_inp')
+            lesson_type = request.POST.get('lesson_type_inp')
+            date = request.POST.get('date_inp')
+            time = request.POST.get('time_inp')
+            status = True
+            student = request.user
+            booking = Bookings(lesson=lesson, lesson_type=lesson_type, date=date, time=time, status=status, student=student)
+            booking.save()
+            return HttpResponseRedirect(reverse('bookings'))
