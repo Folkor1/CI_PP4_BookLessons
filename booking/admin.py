286a9216fe0d69e173d65be5ac1c11aefa03e402
Folkor1 <igorvasiljevs@gmail.com>
2022-10-19 18:10:13 +0000

    Register Bookings class in admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Bookings
 
-# Register your models here.
+
+@admin.register(Bookings)
+class BookingsAdmin(admin.ModelAdmin):
+    """
+    Class for bookings admin model
+    """
+    list_display = ('lesson', 'lesson_type', 'time', 'date', 'status', 'student')
+    search_fields = ['lesson', 'lesson_type', 'date', 'status', 'student']
+    list_filter = ('lesson', 'lesson_type', 'date', 'status', 'student')
