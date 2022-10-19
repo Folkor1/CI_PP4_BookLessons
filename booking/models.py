a2b4e5fe036413c252fe93bd2e2984479f7cf3ef
Folkor1 <igorvasiljevs@gmail.com>
2022-10-19 18:09:08 +0000

    Add functions to return field names

--- booking/models.py
+++ booking/models.py
@@ -14,3 +14,38 @@ class Bookings(models.Model):
     time = models.TimeField()
     status = models.BooleanField(default=False)
     student = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_lesson(self):
+        """
+        Return lesson name
+        """
+        return self.lesson
+
+    def get_lesson_type(self):
+        """
+        Return lesson type name
+        """
+        return self.lesson_type
+
+    def get_date(self):
+        """
+        Return date
+        """
+        return self.date
+
+    def get_time(self):
+        """
+        Return time
+        """
+        return self.time
+
+    def get_student(self):
+        """
+        Return time
+        """
+        return self.student
+        
+    class Meta:
+        ordering = ['-date']
+        verbose_name = 'Booking'
+        verbose_name_plural = 'Bookings'
