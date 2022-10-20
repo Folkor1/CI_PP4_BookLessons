035537504e1b0e152bb12a11dc8d9392c5940054
Folkor1 <igorvasiljevs@gmail.com>
2022-10-20 16:17:31 +0000

    Add path to edit booking date

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.homepage, name="homepage"),
     path('bookings/', views.BookingsView.as_view(), name="bookings"),
     path('manage_bookings/', views.ManageBookingsView.as_view(), name='manage_bookings'),
+    path('edit/<booking_id>', views.edit_booking_date, name="edit"),
 ]
