daad086194f38dabd08f6426de199a66f1543fab
Folkor1 <igorvasiljevs@gmail.com>
2022-10-20 17:06:39 +0000

    Add path for edit booking type

--- booking/urls.py
+++ booking/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('bookings/', views.BookingsView.as_view(), name="bookings"),
     path('manage_bookings/', views.ManageBookingsView.as_view(), name='manage_bookings'),
     path('edit/<booking_id>', views.edit_booking_date, name="edit"),
+    path('edit_type/<booking_id>', views.edit_booking_type, name="edit_type"),
 ]
