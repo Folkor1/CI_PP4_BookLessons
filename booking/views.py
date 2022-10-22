f4af40c4a87a14820085eb2745c9b4ec1f297e65
Folkor1 <igorvasiljevs@gmail.com>
2022-10-22 17:17:44 +0000

    Add filters for completed and upcoming lessons

--- booking/views.py
+++ booking/views.py
@@ -133,13 +133,17 @@ def admin_panel(request):
     admin_theory = Bookings.objects.filter(lesson='Theory').count()
     admin_online = Bookings.objects.filter(lesson_type='Online').count()
     admin_offline = Bookings.objects.filter(lesson_type='Offline').count()
+    admin_completed = Bookings.objects.filter(status='False').count()
+    admin_upcoming = Bookings.objects.filter(status='True').count()
     admin_total = Bookings.objects.all().count()
     context = {
         'admin_piano': admin_piano,
         'admin_theory': admin_theory,
         'admin_online': admin_online,
         'admin_offline': admin_offline,
-        'admin_total': admin_total
+        'admin_total': admin_total,
+        'admin_completed': admin_completed,
+        'admin_upcoming': admin_upcoming
     }
     if not request.user.is_superuser:
         raise PermissionDenied
