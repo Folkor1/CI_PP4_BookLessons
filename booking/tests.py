cd27fdd8dae483ab2d71263c261d6b22361bb3c7
Folkor1 <igorvasiljevs@gmail.com>
2022-10-26 21:24:03 +0000

    Add class to test Bookings.expired() function

--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+import unittest2
+from .models import Bookings
+from django.contrib.auth.models import User
+from datetime import date, time
 
-# Create your tests here.
+
+class TestDates(unittest2.TestCase):
+    """
+    Check Bookings.expired() function
+    """
+
+    def setUp(self):
+        Bookings.objects.create(
+            lesson='Theory',
+            lesson_type='Offline',
+            date='2030-10-24',
+            time='12:00',
+            student_id=1,
+            status=True
+            )
+        Bookings.objects.create(
+            lesson='Piano',
+            lesson_type='Offline',
+            date='2022-10-24',
+            time='12:00',
+            student_id=1,
+            status=True
+            )
+        Bookings.expired()
