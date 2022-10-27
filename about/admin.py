9d6fd42026361a40d92d2a9ab7964f31f8917dfd
Folkor1 <igorvasiljevs@gmail.com>
2022-10-27 20:32:48 +0000

    Add new about to admin

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import About
 
-# Register your models here.
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    """
+    Class for About Admin model
+    """
+    list_display = ('title', 'text', 'image')
