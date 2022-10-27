61ba34d6d32b1684c6a38306e4ebcafb2355da37
Folkor1 <igorvasiljevs@gmail.com>
2022-10-27 19:25:44 +0000

    Move homepage view to home

--- booking/views.py
+++ booking/views.py
@@ -6,13 +6,6 @@ from django.contrib import messages
 from .filters import BookingFilter
 
 
-def homepage(request):
-    """
-    Render the home page
-    """
-    return render(request, "index.html")
-
-
 class BookingsView(generic.ListView):
     """
     Render bookings page
--- bookingpiano/settings.py
+++ bookingpiano/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'booking',
     'adminpanel',
+    'home',
     'django_filters',
 ]
 
